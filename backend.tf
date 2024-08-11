0cb90af4c9e016279d85eb4bafac571f93ac1822
gprabha465 <55969301+gprabha465@users.noreply.github.com>
2024-08-11 18:45:13 +0530

    Delete backend.tf

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "praneeth-terraform-state"
-    key            = "terraform.tfstate"
-    region         = "ap-south-1"
-    encrypt        = true
-  }
-}
